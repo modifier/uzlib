394158bd36b27ac78d3f271f1b18a3c613a86fa1
modifier <the.modifikator@gmail.com>
2023-08-10 23:49:38 +0500

    Add additional code option for svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import '../app.scss';
+
+	import {
+		PUBLIC_ADDITIONAL_CODE
+	} from '$env/static/public';
 </script>
 
 <svelte:head>
 	<title>Книги про Узбекистан</title>
 </svelte:head>
 <slot />
+{PUBLIC_ADDITIONAL_CODE}
